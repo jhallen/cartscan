211a2309c41da71eea685d498ff04341a564d689
Joe Allen <jhallenworld@gmail.com>
2016-02-21 13:54:18 -0500

    license

--- opticon/demo.c
+++ opticon/demo.c
@@ -1,7 +1,21 @@
 /* Item tracking application for Opticon OPH-1005 / OPH-3001
- *
- * by: Joseph Allen
- */
+	    Copyright (C) 2016 Joseph H. Allen
+
+This file is part of Cartscan
+
+Cartscan is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any later
+version.
+
+Cartscan is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+Cartscan; see the file COPYING.  If not, write to the Free Software
+Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "lib.h"
 #include <stdio.h>
